df20974d6f02f94730ed14db02b178674c96a121
Caculuz-cinks <iadeleke01@gmail.com>
2020-08-13 03:44:30 +0100

    Video playing

--- lib/widgets/video_view/video_view.dart
+++ lib/widgets/video_view/video_view.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class VideoView extends StatelessWidget {
+class VideoView extends StatefulWidget {
+  @override
+  _VideoViewState createState() => _VideoViewState();
+}
+
+class _VideoViewState extends State<VideoView> {
+  VideoPlayerController _controller;
+  Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    _controller = VideoPlayerController.network(
+        'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4');
+
+    _initializeVideoPlayerFuture = _controller.initialize();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ConstrainedBox(
@@ -13,6 +37,19 @@ class VideoView extends StatelessWidget {
           // color: Color(0xffF8F7F9),
           color: Colors.black,
           borderRadius: BorderRadius.circular(12),
+        ),
+        child: FutureBuilder(
+          future: _initializeVideoPlayerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              );
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
         ),
       ),
     );
